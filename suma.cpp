f1c08cdd86573aac562889525a1ded5fcc5e4ebd
Jhalmarlitardo <jhalmar.litardo@utelvt.edu.ec>
2022-09-09 08:42:19 -0500

    Suma.cpp Modificado

--- suma.cpp
+++ suma.cpp
@@ -2,9 +2,13 @@
 using namespace std;
 int main()
 {
-	float A,B,C;
-	cin>>A>>B;
-	C=A+B;
-	cout<<C;
+	float x,y,z;
+	cout<<"Ingrese 2 numero separados de espacio x y :";
+	cout<<"Ingrese 2 numero x y :";
+	cin>>x>>y;
+	z=x+y;
+	cout<<"El resulado es :";
+	cout<<"El resutlado es: ";
+	cout<<z;
 	return 0;
 }
